36a88835f667339ee1f7e10ab8fd0b1fc15dea25
deskach <dzianis.kachanau@gmail.com>
2016-07-09 08:03:01 +1000

    made search_bar a stateful component.

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return <input />
-};
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {term: ''};
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={ event => this.setState({ term: event.target.value }) }
+        />
+        Value of the input: {this.state.term}
+      </div>
+    );
+  }
+}
 
 export default SearchBar;
 
@@ -10,4 +26,17 @@ export default SearchBar;
  * - even though React is not directly used here, it however will be used after jsx->js transformation,
  * that is why is should be imported in all jsx modules.
  * - export default means that by default SearchBar is exposed to the outer world.
+ * -  SearchBar is a class component (also called 'smart' component). It has mandatory render() method
+ * and communicates to other components.
+ * - name events as 'onInputChange' or 'handleInputChange' is <on}handle><tag><event>
+ *
+ *   State is a plain Javascript object that is used to record and react to user events. Each class-based
+ *   component has its own state object. When state changes the component re-renders and all its sub-components.
+ *   To initialize state with set the property state to a plain js-object inside the constructor of a smart component.
+ *   State can only be assigned using '=' inside a constructor. Everywhere else it is handled using this.setState(val)
+ *   method - is. never change state like this:
+ *      this.state.term = newVal -- that is BAD!!! NEVER DO THIS
+ *
+ *   <input value={this.state.term}... is a "controlled component", because its value depends on state.
+ *
  */
