c01b79915f6802a22982d191e45b7f8d12cbb6ab
deskach <dzianis.kachanau@gmail.com>
2016-07-09 11:47:41 +1000

    enhanced index.js to be a class-component used to stroe videos retreived from youtube

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
-// const API_KEY = "AIzaSyCg_lS0N0cyWuUbwevaWekrFfSZH88QyDA";
+const API_KEY = "AIzaSyCg_lS0N0cyWuUbwevaWekrFfSZH88QyDA";
 
 // 1. Create a react component which can generate HTML
-const App = () => {
-  return <div>
-    <SearchBar />
-  </div>;
-};
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    var that = this;
+
+    this.state = {videos: []};
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      that.setState({videos}); //<=> that.setState({videos: videos}); //ES6 trick
+    });
+  }
+
+  render() {
+    return <div>
+      <SearchBar />
+    </div>;
+  }
+}
 
 // 2. Add the component's html into the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
 
 /* NOTES:
  - const is ES6 feature;
+ - in react the root component is the one allowed to do data-fetching from (Redux, REST-api etc);
+ -
  */
