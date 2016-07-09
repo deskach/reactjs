2fc31e9b231b3553a391f86f92dd00b00c3c55f6
deskach <dzianis.kachanau@gmail.com>
2016-07-09 16:17:12 +1000

    Introduced VideoListItem and used it in VideoList component

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem video={video} />
+  });
+
   return (
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoItems}
     </ul>
   )
 };
@@ -13,5 +18,6 @@ export default VideoList;
 /*
  * NOTES:
  * - <ul className...> is a react syntax that allows for avoiding conflicts with es6 class keyword
- *
+ * - prefer using Array.map rather than for loop;
+ * - React can render a list of components (like videoItems)
  */
