457f5e386a3dcf181a59be181106d3116a1f7de9
deskach <dzianis.kachanau@gmail.com>
2016-07-11 00:31:15 +1000

    Loaded a video into the VideoDetail

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyCg_lS0N0cyWuUbwevaWekrFfSZH88QyDA";
 
@@ -24,6 +25,7 @@ class App extends React.Component {
   render() {
     return <div>
       <SearchBar />
+      <VideoDetail video={this.state.videos[0]} />
       <VideoList videos={this.state.videos} />
     </div>;
   }
