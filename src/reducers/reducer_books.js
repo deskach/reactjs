af540c48e59d2cadd369eb905956077cbc66ecf9
deskach <dzianis.kachanau@gmail.com>
2016-07-11 09:32:06 +1000

    introduced BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function BooksReducer() {
+  return [
+    {title: "Black & white"},
+    {title: "React & Redux"},
+    {title: "Flask foondamentals"},
+    {title: "REST architecture"},
+    {title: "Microwave frameworks"}
+  ];
+};
