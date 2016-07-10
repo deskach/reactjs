77d928643e9e9eaa2e9849facf1fd00cf037ee24
deskach <dzianis.kachanau@gmail.com>
2016-07-11 00:04:54 +1000

    Implemented VideoListItem and enhanced VideoList to use col-md-8

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,11 +3,11 @@ import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
   const videoItems = props.videos.map((video) => {
-    return <VideoListItem video={video} />
+    return <VideoListItem key={video.etag} video={video} />
   });
 
   return (
-    <ul className="col-md-4 list-group">
+    <ul className="col-md-8 col-xs-12 list-group">
       {videoItems}
     </ul>
   )
@@ -20,4 +20,5 @@ export default VideoList;
  * - <ul className...> is a react syntax that allows for avoiding conflicts with es6 class keyword
  * - prefer using Array.map rather than for loop;
  * - React can render a list of components (like videoItems)
+ * - key={video.etag} is required to allow react generate a unique id for each item
  */
