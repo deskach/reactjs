17f02ef0954a5774c5e49a4ca66560bb4a85e160
deskach <dzianis.kachanau@gmail.com>
2016-07-13 11:28:21 +1000

    Connected action creator to the container

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
 class BookList extends React.Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          onClick={ () => this.props.selectBook(book)}
+          key={book.title} 
+          className="list-group-item">{book.title}
+        </li>
       );
     });
   }
@@ -19,15 +25,23 @@ class BookList extends React.Component {
   }
 
   static mapStateToProps(state) {
+    // Whatever is returned will show up as BookList props
     return {
       books: state.books
     };
   }
+
+  // Anything returned from this function will end up as props on the BookList container
+  static mapDispatchToProps(dispatch) {
+    // Whenever selectBook is called, result should be passed to all reducers
+    return bindActionCreators({selectBook: selectBook}, dispatch);
+  }
 }
 
-export default connect(BookList.mapStateToProps)(BookList);
+// Promote BookList from a component to a container. It needs to know about dispatch method selectBook.
+export default connect(BookList.mapStateToProps, BookList.mapDispatchToProps)(BookList);
 
 /* NOTES:
-* connect function allows for automatic props update of the BookList component whenever state changes;
-*
+ * connect function allows for automatic props update of the BookList component whenever state changes;
+ *
  */
