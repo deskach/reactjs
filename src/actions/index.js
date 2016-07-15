708c16deb3090dd63bab2904396abfbf5329bd5b
deskach <dzianis.kachanau@gmail.com>
2016-07-15 14:39:01 +1000

    made use of fetchWeather action from SearchBar component

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = 'bf65376f6a8fa19fc8d6e95e798a06a0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,22 +1,33 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchWeather} from '../actions/index';
 
-export default class SearchBar extends React.Component {
+class SearchBar extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = { term: '' };
 
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
-    //^this is needed to make 'this' reference correct object on onChange={this.onInputChange} callback execution
+    //^these is needed to make 'this' reference correct object during onChange={this.onInputChange} callback executions
   }
 
   onInputChange(event) {
     this.setState({term: event.target.value});
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           className="form-control"
           value={this.state.term}
@@ -28,4 +39,11 @@ export default class SearchBar extends React.Component {
       </form>
     );
   }
+
+  static mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+  }
 }
+
+export default connect(null, SearchBar.mapDispatchToProps)(SearchBar);
+//^null here because this container does not use state
