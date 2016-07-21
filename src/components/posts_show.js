369960b683a3f4936871f55cf6c769b8101d8c4d
deskach <dzianis.kachanau@gmail.com>
2016-07-22 04:48:02 +1000

    implemented post:id showing

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index';
 
 class PostsShow extends React.Component {
+  componentWillMount() {
+    this.props.fetchPost(this.props.params.id);
+  }
+
   render() {
+    if (!this.props.post) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>Show post {this.props.params.id}</div>
+      <div>
+        <h3>{this.props.post.title}</h3>
+        <h6>Categories: {this.props.post.categories}</h6>
+        <p>{this.props.post.content}</p>
+      </div>
     );
   }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+  return {post: state.posts.post};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
