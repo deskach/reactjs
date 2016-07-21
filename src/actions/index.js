5026c9af74386cdc2d88626d2ba74fd6c48cc4a0
deskach <dzianis.kachanau@gmail.com>
2016-07-22 02:27:00 +1000

    created a reduxForm

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 
-const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-const API_KEY = '?key=laskjddf;ladsjfs;asfasdf923487ewrioql';
+const ROOT_URL = 'http://reduxblog.herokuap.com/api';
+const API_KEY = '?key=laskjddfladsjfsasfqwr13as2df923487ewrioql';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
   return {
-    tyoe: FETCH_POSTS,
+    type: FETCH_POSTS,
     payload: request
   };
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+import {fetchPosts} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsIndex extends Component {
   componentWillMount() {
-    //^ this function is called when the component about to be rendered the 1st time
-    console.log('this would be a good time to call an action creator to retrieve posts');
+    //this function is called when the component is about to be rendered the 1st time
+    this.props.fetchPosts();
   }
 
   render() {
     return (
-      <div>List of BLOG posts</div>
+      <div>
+        <div className="text-xs-right">
+          <Link to="/posts/new" className="btn btn-primary">
+            Add a Post
+          </Link>
+        </div>
+        List of BLOG posts
+      </div>
     );
   }
 }
 
-export default PostsIndex;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchPosts}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(PostsIndex);
