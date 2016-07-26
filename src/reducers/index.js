c7fa0e07c082b25fee67c5a7c83503ebc31813e0
deskach <dzianis.kachanau@gmail.com>
2016-07-26 17:25:33 +1000

    introduced action/reducer for authentication

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,17 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from "redux";
+import {CHANGE_AUTH} from "../actions/types";
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  authenticated: authenticationReducer
 });
 
 export default rootReducer;
+
+export function authenticationReducer(state = false, action) {
+  switch (action.type) {
+    case CHANGE_AUTH:
+      return action.payload;
+  }
+
+  return state;
+}
