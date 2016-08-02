0d5bfa8d80c65d8c4cebfed1e382276b6240d1d7
deskach <dzianis.kachanau@gmail.com>
2016-08-03 08:22:03 +1000

    imtroduced user creation via http-POST request

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,33 @@
+const User = require('../models/user');
+
 exports.signup = function (req, res, next) {
-  res.send({success: 'true'});
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // See if a user with the given email exists
+  User.findOne({email}, (err, existingUser) => {
+    if (err) {
+      return next(err);
+    }
+
+    if (existingUser) {
+      //422 Unprocessable entity http status code
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    // If a user with email does exist, return an error
+    const user = new User({email, password});
+
+    user.save((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.json(user);
+    });
+
+    // Otherwise create & save a user record
+
+    // Respond to the request indicating the user was created
+  });
 };
