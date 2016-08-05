81d9e02f277edf3cf5e21b4ec0bd969688e842d3
deskach <dzianis.kachanau@gmail.com>
2016-08-06 06:34:13 +1000

    introduced an auth-reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {browserHistory} from "react-router";
+import {AUTH_USER} from "./types";
 
 const ROOT_URL = 'http://localhost:3090';
 
@@ -7,6 +8,8 @@ export function signin({email, password}) {
   return (dispatch) => {
     axios.post(`${ROOT_URL}/signin`, {email, password})
       .then(response => {
+        dispatch({type: AUTH_USER});
+
         browserHistory.push('/feature');
       })
       .catch(() => {
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,21 @@
 import {combineReducers} from "redux";
 import {reducer as form} from "redux-form";
+import {AUTH_USER, UNAUTH_USER} from "../actions/types";
+
+function authReducer(state = {}, action) {
+  switch (action.type) {
+    case AUTH_USER:
+      return {...state, authenticated: true};
+    case UNAUTH_USER:
+      return {...state, authenticated: false};
+  }
+
+  return state;
+}
 
 const rootReducer = combineReducers({
-  form
+  form,
+  auth: authReducer
 });
 
 export default rootReducer;
