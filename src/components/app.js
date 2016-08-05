b66f041608778ab3788830f88e62ccf85f426f2c
deskach <dzianis.kachanau@gmail.com>
2016-08-05 16:40:08 +1000

    brushed up form's layout

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ import Header from "./header";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
         <Header />
         {this.props.children}
       </div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,11 @@ import React, {Component} from "react";
 class Header extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <nav className="navbar navbar-default">
-          <ul className="nav navbar-nav">
-            <li className="nav-item"><a href="#">Sign In</a></li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="navbar navbar-default">
+        <ul className="nav navbar-nav">
+          <li className="nav-item"><a href="#">Sign In</a></li>
+        </ul>
+      </nav>
     )
   }
 }
