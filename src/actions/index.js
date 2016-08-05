ba7c2300c453287f7a90757dcca17b6cf1e87ca8
deskach <dzianis.kachanau@gmail.com>
2016-08-05 17:04:49 +1000

    installed redux-thunk

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+
+
+export function signInUser({email, password}) {
+  return (dispatch) => {
+  }
+}
+
+/*
+  redux-thunk gives us access to the dispatch function which we can call to
+  propagate action to the reducers at any time we want
+ */
