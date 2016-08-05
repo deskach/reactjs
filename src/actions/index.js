16288e34bce595815021041b8574a44a174d2771
deskach <dzianis.kachanau@gmail.com>
2016-08-06 06:05:28 +1000

    connected front-end signin with the server side

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,10 @@
+import axios from "axios";
 
+const ROOT_URL = 'http://localhost:3090';
 
-export function signInUser({email, password}) {
+export function signin({email, password}) {
   return (dispatch) => {
+    axios.post(`${ROOT_URL}/signin`, {email, password});
   }
 }
 
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,9 +1,12 @@
 import React, {Component} from "react";
 import {reduxForm} from "redux-form";
+import * as actions from "../../actions";
 
 class Signin extends Component {
   handleFormSubmit({email, password}) {
     console.log(email, password);
+
+    this.props.signin({email, password});
   }
 
   render() {
@@ -29,4 +32,4 @@ class Signin extends Component {
 export default reduxForm({
   form: 'signin',
   fields: ['email', 'password']
-})(Signin);
+}, null, actions)(Signin);
