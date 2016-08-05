2c18f55c0d565dd4e98d7ee01a62d010514f4eb5
deskach <dzianis.kachanau@gmail.com>
2016-08-06 06:27:14 +1000

    implemented navigation to /feature on successful signin

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,16 @@
 import axios from "axios";
+import {browserHistory} from "react-router";
 
 const ROOT_URL = 'http://localhost:3090';
 
 export function signin({email, password}) {
   return (dispatch) => {
-    axios.post(`${ROOT_URL}/signin`, {email, password});
+    axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+        browserHistory.push('/feature');
+      })
+      .catch(() => {
+      });
   }
 }
 
