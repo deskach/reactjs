35a5b9a7fbea7f3503713f6b206fac4f4d63cd93
deskach <dzianis.kachanau@gmail.com>
2016-08-06 08:39:01 +1000

    enhanced header with sign in/ sign out

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,45 @@
 import React, {Component} from "react";
+import {connect} from "react-redux";
+import {Link} from "react-router";
 
 class Header extends Component {
+  renderLinks() {
+    let result = [
+      <li className="nav-item" key={1}>
+        <Link className="nav-link" to="/signin">Sign In</Link>
+      </li>,
+      <li className="nav-item" key={2}>
+        <Link className="nav-link" to="/signup">Sign Up</Link>
+      </li>
+    ];
+
+    if (this.props.authenticated) {
+      result = (
+        <li className="nav-item">
+          <Link className="nav-link" to="/signout">Sign Out</Link>
+        </li>
+      );
+    }
+
+    return result;
+  }
+
   render() {
     return (
       <nav className="navbar navbar-default">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item"><a href="#">Sign In</a></li>
+          {this.renderLinks()}
         </ul>
       </nav>
     )
   }
+
+  static mapState2Props(state) {
+    return {
+      authenticated: state.auth.authenticated
+    };
+  }
 }
 
-export default Header;
+export default connect(Header.mapState2Props)(Header);
