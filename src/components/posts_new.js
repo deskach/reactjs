86309fe62145a60df52e8c5a26e22b15f92b15a1
deskach <dzianis.kachanau@gmail.com>
2017-09-24 07:29:42 +1000

    Added react-router-dom Switch

---
+++ src/components/posts_new.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import {connect} from 'react-redux';
+// import {fetchPosts} from "../actions/index";
+
+
+class PostsNew extends React.Component {
+    render() {
+        return (
+            <div>Posts New</div>
+        );
+    }
+}
+
+// export default connect(PostsNew.mapStateToProps, {fetchPosts})(PostsNew);
+export default PostsNew;
--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
 import reducers from './reducers';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import PostsIndex from "./components/posts_index";
 import promise from 'redux-promise';
+import PostsNew from "./components/posts_new";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
         <BrowserRouter>
-            <div>
+            <Switch>
+                <Route path={"/posts/new"} component={PostsNew}/>
                 <Route path={"/"} component={PostsIndex}/>
-            </div>
+            </Switch>
         </BrowserRouter>
     </Provider>
     , document.querySelector('.container')
