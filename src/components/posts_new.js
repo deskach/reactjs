6efd92fd879c0619abf9e8e0f800e054a54136ac
deskach <dzianis.kachanau@gmail.com>
2017-09-24 14:09:23 +1000

    Used redux-form in PostsNew component

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,15 +1,31 @@
 import React from 'react';
 // import {connect} from 'react-redux';
 // import {fetchPosts} from "../actions/index";
+import {Field, reduxForm} from 'redux-form';
 
 
 class PostsNew extends React.Component {
+    renderField(field) {
+        return (
+            <div>
+                <label>{field.label}</label>
+                <input type={'text'}{...field.input} className={'form-control'}/>
+            </div>
+        );
+    }
+
     render() {
         return (
-            <div>Posts New</div>
+            <form className={'form-group'}>
+                <Field label={'Title'} name={'title'} component={this.renderField}/>
+                <Field label={'Tags'} name={'tags'} component={this.renderField}/>
+                <Field label={'Post Content'} name={'content'} component={this.renderField}/>
+            </form>
         );
     }
 }
 
 // export default connect(PostsNew.mapStateToProps, {fetchPosts})(PostsNew);
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm',
+})(PostsNew);
